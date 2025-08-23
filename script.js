b238aa0d6b6aa95d4369c560a8b7066c276d1faf
smileywrld <ismaileyyunusa@gmail.com>
2025-08-23 23:34:53 +0100

    Add current date retrieval logic for age calculation

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const userDay = document.getElementById("day").value;
 const userMonth = document.getElementById("month").value;
 const userYear = document.getElementById("year").year;
+
+const currentDate = new Date();
+const currentDay = currentDate.getDate();
+const currentMonth = currentDate.getMonth() + 1;
+const currentYear = currentDate.getFullYear();
+console.log(currentMonth);
