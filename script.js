05a3379330aa23a379211077e01b5b9b493cdc6e
smileywrld <ismaileyyunusa@gmail.com>
2025-08-23 23:45:39 +0100

    Add validation checks for user input fields to ensure all fields are filled and values are within valid ranges

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ const userDay = document.getElementById("day").value;
 const userMonth = document.getElementById("month").value;
 const userYear = document.getElementById("year").year;
 
+if (userDay || userMonth || userYear == "") {
+	console.log("all fields are required");
+} else if (userMonth < 1 || userMonth > 12) {
+	console.log("invalid month");
+} else if (userDay < 1 || userDay > 32) {
+	console.log("invalid day");
+} else if (userYear > currentYear) {
+	console.log("Year cannot be in the future");
+}
+
 const currentDate = new Date();
 const currentDay = currentDate.getDate();
 const currentMonth = currentDate.getMonth() + 1;
